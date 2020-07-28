f32b5de3281bf7147f9e97275723d72ea18ea005
YaelBadian <yael@proteantecs.com>
2020-07-28 16:55:13 +0300

    read first dataset

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hi Omer")
+import gzip
+import json
+import tarfile
+
+with gzip.GzipFile('aotm2011_playlists.json.gz', 'r') as fin:
+    json_bytes = fin.read()
+
+json_str = json_bytes.decode('utf-8')
+data = json.loads(json_str)
+print(data)
